6414296b51906bcdb9eb952e1d6d6f360ff63efc
beryl452 <beryl.houessou@gmail.com>
2022-08-25 21:58:52 +0000

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    import sys
     from calculator_1 import add, sub, mul, div
-    from sys import argv
-    if len(argv) != 4:
-        print("Usage:", argv[0], "<a> <operator> <b>")
-        exit(1)
-
-    op = argv[2]
-    f = {"+": add, "-": sub, "*": mul, "/": div}
-    if op not in f:
+    ls = ['+', '-', '*', '/']
+    if (len(sys.argv) != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    elif sys.argv[2] not in ls:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-
-    a = int(argv[1])
-    b = int(argv[3])
-    print("{:d} {:s} {:d} = {:d}".format(a, op, b, f[op](a, b)))
-
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if sys.argv[2] == ls[0]:
+        print("{:n} + {:n} = {:n}".format(a, b, add(a, b)))
+        sys.exit(0)
+    elif sys.argv[2] == ls[1]:
+        print("{:n} - {:n} = {:n}".format(a, b, sub(a, b)))
+        sys.exit(0)
+    elif sys.argv[2] == ls[2]:
+        print("{:n} * {:n} = {:n}".format(a, b, mul(a, b)))
+        sys.exit(0)
+    elif sys.argv[2] == ls[3]:
+        print("{:n} / {:n} = {:n}".format(a, b, div(a, b)))
+        sys.exit(0)
