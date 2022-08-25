d9314e6e316ddafda97018f3d3eb60796a2da7c1
beryl452 <beryl.houessou@gmail.com>
2022-08-25 20:16:56 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    userin = argv[1:]
-    argc = len(userin)
-    print("{:d} {:s}{:s}".
-          format(argc,
-                 "arguments" if (argc) is not 1 else "argument",
-                 "." if (argc) is 0 else ":"))
-    for index, arg in enumerate(userin):
-        print("{:d}: {:s}".format(index + 1, arg))
+    import sys
+    argv = sys.argv[1:]
+    i = 1
+    j = len(argv)
+    if j == 0:
+        print("{:d} arguments.".format(j))
+    elif len(argv) == 1:
+        print("{:d} argument:".format(j))
+        print("{:d}: {:s}".format(i, sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(j))
+        while i <= j:
+            print("{:d}: {:s}".format(i, sys.argv[i]))
+            i += 1
