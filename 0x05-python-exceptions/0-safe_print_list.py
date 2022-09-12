f484126ed60b8b4999b888b3a982e6a092e9ab0f
beryl452 <beryl.houessou@gmail.com>
2022-09-12 19:28:55 +0000

    1-safe_print_integer.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,9 @@ def safe_print_list(my_list=[], x=0):
 	while n_el < x:
 		try:
 			print("{}".format(my_list[n_el]),end="")
-			n_el += 1
 		except IndexError:
 			break
+	n_el += 1
 	print()
 	return n_el
 
