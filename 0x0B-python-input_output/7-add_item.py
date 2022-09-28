3f8ddbf2d7e5143a4b81a924b2080e002bed9bf8
beryl452 <beryl.houessou@gmail.com>
2022-09-28 20:51:41 +0000

    10-student.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,7 @@
-
 #!/usr/bin/python3
 """
 Module 7-add_item
 Contains function that adds and saves to Python obj to JSON file; loads objects
-# run with ./7-add_item.py
-#
-# cat add_item.json ; echo ""
-# expect output: []
-#
-# ./7-add_item.py some random args
-# cat add_item.json ; echo ""
-# expect output: ["some", "random", "args"]
 """
 
 
