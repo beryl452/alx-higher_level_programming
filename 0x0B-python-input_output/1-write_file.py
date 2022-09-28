5af1db8e4f1f70a7a4adb79900e037ccff03bb93
beryl452 <beryl.houessou@gmail.com>
2022-09-28 20:25:25 +0000

    3-to_json_string.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """
-Module 1-number_of_lines
-Contains function that returns number of lines in text file
+Contains the function "wrtie_file"
 """
 
 
-def number_of_lines(filename=""):
-    """Return number of lines in text file"""
-    with open(filename, mode="r", encoding="utf-8") as f:
-        lines = 0
-        for line in f:
-            lines += 1
-    return lines
+def write_file(filename="", text=""):
+    """returns the number of chars written to "filename" from "text" """
+    with open(filename, 'w', encoding='utf=8') as f:
+        return f.write(text)
 
