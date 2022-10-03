ecedb77f5fa4bebe9fd1892d872acb377204550e
beryl452 <beryl.houessou@gmail.com>
2022-10-03 05:36:44 +0000

    base.py

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module contains class Base
+"""
+
+
+class Base():
+    """
+    defines class Base
+    Class Attributes:
+        __nb_objects
+    Methods:
+        __init__(self, id=None)
+    """
+    __nb_objects = 0
+
+    def __init__(self, id = None):
+        """Initialise id, increment class attribute if no id and set as id"""
+        if id:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
