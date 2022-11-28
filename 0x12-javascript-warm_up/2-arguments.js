7ee4781b0d63c02b0fc7e03ebbf5ad66185d5916
beryl452 <beryl.houessou@gmail.com>
2022-11-28 20:03:09 +0000

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
 
-if (process.argv[1] === undefined) { console.log('No argument'); } else if (process.argv[2] === undefined) { console.log('Argument found'); } else { console.log('Arguments found'); }
+const argc = process.argv.length;
+
+if (argc > 2) {
+  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
+} else {
+  console.log('No argument');
+}
 
