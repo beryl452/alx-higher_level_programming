c8eb249eb6393e21782621983ee2d024a19a8bcd
beryl452 <beryl.houessou@gmail.com>
2022-12-04 20:50:43 +0000

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Lists all states from the database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+def main():
+    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    [print(state) for state in cur.fetchall() if state[1][0]=='N']
+    cur.close()
+    conn.close()
+
+if __name__ == "__main__":
+    main()
