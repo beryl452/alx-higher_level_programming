d6939c7d07441eb73ff21e71a45f0ee40628d38c
beryl452 <beryl.houessou@gmail.com>
2022-12-22 15:15:04 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,10 +11,10 @@ def get_status():
     with urllib.request.urlopen(url) as response:
         html = response.read()
         html_utf8 = html.decode("utf-8")
-        print("Body response:$")
-        print("\t- type: {}$".format(type(html)))
-        print("\t- content: {}$".format(html))
-        print("\t- utf8 content: {}$".format(html_utf8))
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html_utf8))
 
 if __name__ == "__main__":
     get_status()
