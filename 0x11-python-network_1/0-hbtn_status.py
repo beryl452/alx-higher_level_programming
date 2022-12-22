ade9cba18d1d1a0b27cce4255f6367ba67fffc8f
beryl452 <beryl.houessou@gmail.com>
2022-12-22 15:12:56 +0000

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,14 +3,18 @@
     Fetches https://alx-intranet.hbtn.io/status
 """
 import urllib.request
-
-if __name__ == "__main__":
+def get_status():
+    """
+        fetches https://alx-intranet.hbtn.io/status
+    """
     url = "https://alx-intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         html = response.read()
         html_utf8 = html.decode("utf-8")
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html_utf8))
+        print("Body response:$")
+        print("\t- type: {}$".format(type(html)))
+        print("\t- content: {}$".format(html))
+        print("\t- utf8 content: {}$".format(html_utf8))
 
+if __name__ == "__main__":
+    get_status()
--- 0x11-python-network_1/test.py
+++ 0x11-python-network_1/test.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
 """
-    Fetches https://alx-intranet.hbtn.io/status
+fetch https://intranet.hbtn.io/status; display response
 """
+
 import urllib.request
-def get_status():
-    """
-        Retrieves the status of the intranet using the urllib module.
-    """
-    url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        html_utf8 = html.decode("utf-8")
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html_utf8))
 
 if __name__ == "__main__":
-    get_status()
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
 
